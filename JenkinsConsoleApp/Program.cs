0b550992a2f517957e6ca06a25095af13b354760
n2y2coder <n2y2coder@users.noreply.github.com>
2019-06-14 00:58:04 -0400

    Changes made by me

--- JenkinsConsoleApp/Program.cs
+++ JenkinsConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace JenkinsConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Naveen!");
         }
     }
 }
